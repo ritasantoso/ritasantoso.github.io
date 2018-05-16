656afa481fb627120e2e878962119aa95adbf46d
Parth Sarin <parthsarin@gmail.com>
2018-05-16 10:23:27 -0500

    change fonts and add hover on img

--- update_pictures.py
+++ update_pictures.py
@@ -14,6 +14,15 @@ for picture in picture_data:
         currentYear = picture["year"]
         print("<h3>" + str(currentYear) + "</h3>")
 
-    print("<a href=\"/images/" + picture["filename"].replace('"', '&quot;') + "\" title=\"" + picture["title"].replace('"', '&quot;') + "\">")
-    print(" <img src=\"/images/"  + picture["filename"].replace('"', '&quot;') + "\"></img>")
+    lightbox_description = picture["title"].replace('"', '&quot;') + ", " + picture["medium"].replace('"', '&quot;') + ", " + picture["details"].replace('"', '&quot;')
+
+    print("<a href=\"/images/" + picture["filename"].replace('"', '&quot;') + "\" title=\"" + lightbox_description + "\">")
+    print(" <div class=\"gallery-img-container\">")
+    print("  <img class=\"gallery-img\" src=\"/images/"  + picture["filename"].replace('"', '&quot;') + "\"></img>")
+    print("  <div class=\"gallery-text\">")
+    print("     <span class=\"title\">" + picture["title"].replace('"', '&quot;') + "</span>")
+    print("     <span class=\"medium\">" + picture["medium"].replace('"', '&quot;') + "</span>")
+    print("     <span class=\"details\">" + picture["details"].replace('"', '&quot;') + "</span>")
+    print("  </div>")
+    print(" </div>")
     print("</a>")
